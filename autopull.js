0c57f49f98651671c1e0dea72817cdc34f65cbc7
Burgil‬‏ <41600149+burgil@users.noreply.github.com>
2025-01-11 14:08:41 +0200

    Update server log message to include specific version number in AutoPull startup

--- autopull.js
+++ autopull.js
@@ -114,5 +114,5 @@ for (const resourceName in config.resources) {
 
 // Start the server
 app.listen(config.port, () => {
-    console.log(`AutoPull V2 server is running on port ${config.port}`);
+    console.log(`AutoPull v2.0.0 server is running on port ${config.port}`);
 });
